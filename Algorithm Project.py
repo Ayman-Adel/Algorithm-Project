20f163c3571ce0634b97ef762ee2775aa09c6438
Mohamed-Samahi <mohamedsamahi2021@gmail.com>
2023-05-14 21:28:41 +0300

    job sequence algorithm with random jobs input

--- Algorithm Project.py
+++ Algorithm Project.py
@@ -1,4 +1,3 @@
-
 #QA Function
 def schedule_job(jobs) :
     #sorting the jobs by Profit
@@ -17,14 +16,40 @@ def schedule_job(jobs) :
 
      return num_jobs_done,maxprofit
 
+#QA Test with randomly generated jobs 
+def JobSequence():
+ import random
+
+ jobs = []
+ for i in range(5):
+    jobs.append({'id': i, 'deadline': random.randint(1, 10), 'profit': random.randint(10, 100)})
+
+ jobs.sort(key=lambda x: x['profit'], reverse=True)
+ print('Jobs : ',jobs)
+
+ jobsDone = []
+ time = 0
+
+ for job in jobs:
+    if time < job['deadline']:
+        time += 1
+        jobsDone.append(job)
+
+ print('Jobs Done: ',jobsDone)
+
+ print('Num of Jobs Done: ', len(jobsDone))
+ print('Total Profit: ', sum(job['profit'] for job in jobsDone))
 
 
 def main():
     
-    #QA
+    # QA test with predefined fixed input
     Q1jobs = [(1,4,20),(2,1,10),(3,1,40),(4,1,30)]
     num_jobs_done,maxprofit = schedule_job(Q1jobs)
     print('QA Number of jobs done = ' , num_jobs_done,'\nMax Profit = ',maxprofit )
+
+    # QA test with random jobs
+    # JobSequence()
 
 
 
