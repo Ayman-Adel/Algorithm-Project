d0c1ec2df34902254cb5c33547a0841de77a9754
Ayman-Adel <aymanadel841@gmail.com>
2023-05-15 22:47:34 +0300

    Make QA Function Code cleaner

--- Algorithm Project.py
+++ Algorithm Project.py
@@ -1,20 +1,16 @@
 #QA Function
 def schedule_job(jobs) :
+     
     #sorting the jobs by Profit
      jobs.sort(key=lambda x: x[2],reverse = True)
-    # initialize variables
-     current_time = 0
-     num_jobs_done = 0
-     maxprofit = 0
+    # initialize schedule list for jobs done
+     schedule = []
      # loop through the jobs
-     for i in jobs :
-        # if Current time unit is less than the deadline of the job 
-        if current_time < i[1] :
-            current_time+=1
-            num_jobs_done+=1
-            maxprofit += i[2]
+     for job in jobs:
+      if len(schedule) < job[1]:
+       schedule.append(job)
+     return len(schedule), sum([job[2] for job in schedule])
 
-     return num_jobs_done,maxprofit
 
 #QA Test with randomly generated jobs 
 def JobSequence():
@@ -45,8 +41,8 @@ def main():
     
     # QA test with predefined fixed input
     Q1jobs = [(1,4,20),(2,1,10),(3,1,40),(4,1,30)]
-    num_jobs_done,maxprofit = schedule_job(Q1jobs)
-    print('QA Number of jobs done = ' , num_jobs_done,'\nMax Profit = ',maxprofit )
+    num_jobs,maxprofit = schedule_job(Q1jobs)
+    print('QA Number of jobs done = ' , num_jobs,'\nMax Profit = ',maxprofit )
 
     # QA test with random jobs
     # JobSequence()
